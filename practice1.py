8be45a53da173d6d481e053a5be81c35f6631d03
HombreValiente <dohohun@naver.com>
2022-02-28 18:12:17 +0900

    second commit

--- practice1.py
+++ practice1.py
@@ -40,7 +40,34 @@
 # print(a)
 
 # 소수점 표현하기
-a = "%0.4f" % 3.42134234
+# a = "%0.4f" % 3.42134234
+# print(a)
+# b = "%10.4f" % 3.42134234
+# print(b)
+
+# format 함수를 이용한 포맷팅
+# a = "I eat {0} apples".format(3)
+# print(a)
+# a = "I eat {0} apples".format("five")
+# print(a)
+# number = 10
+# a = "I eat {0} apples".format(number)
+# print(a)
+# a = "I eat {number} apples. so I was sick for {days} days".format(number=10, days=3)
+# print(a)
+# a = "I eat {0} apples. so I was sick for {day} days".format(10, day=3)
+# a = "{0:<10}".format("hi")   # 왼쪽으로 정렬
+# print(a)
+# b = "{0:>10}".format("hi")   # 오른쪽으로 정렬
+# print(b)
+# a = "{0:^10}".format("hi")     # 가운데로 정렬
+# print(a)
+# 공백 채우기
+# a = "{0:=^10}".format("hi")
+# print(a)
+# b = "{0:!<10}".format("hi")
+# print(b)
+# 소수점 표현
+y = 3.4231234
+a = "{0:0.4f}".format(y)
 print(a)
-b = "%10.4f" % 3.42134234
-print(b)
