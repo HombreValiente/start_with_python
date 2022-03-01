12bc61b178a697ef291e71e3cb44bf2d5c946419
HombreValiente <dohohun@naver.com>
2022-03-01 15:46:59 +0900

    third commit

--- practice1.py
+++ practice1.py
@@ -68,6 +68,87 @@
 # b = "{0:!<10}".format("hi")
 # print(b)
 # 소수점 표현
-y = 3.4231234
-a = "{0:0.4f}".format(y)
-print(a)
+# y = 3.4231234
+# a = "{0:0.4f}".format(y)
+# print(a)
+# {또는} 문자 표현하기
+# a = "{{and}}".format()
+# print(a)
+
+# f 문자열 포맷팅
+# name = " 홍길동 "
+# age = 30
+# a = f'나의 이름은 {name}이고, 나이는 {age + 1}입니다.'
+# print(a)
+# d = {'name':'홍길동', 'age':'30'}
+# a = f'나의 이름은 {d["name"]}입니다. 나이는 {d["age"]}입니다.'
+# print(a)
+# a = f'{"hi":<10}'
+# b = f'{"hi":>10}'
+# c = f'{"hi":^10}'
+# d = f'{"hi":=^10}'
+# e = f'{"hi":!>10}'
+# print(a, b, c, d, e)
+# a = f'{"python":!^12}'
+# print(a)
+
+# 문자열 관련 함수
+# 문자 개수 세기(count)
+# a = "hobby"
+# c = a.count('b')
+# print(c)
+
+# 위치 알려주기(find) # 찾는 문자나 문자열이 없으면 -1을 반환
+# a = "python is the best choice"
+# c = a.find('b') 
+# d = a.find('k')
+# print(c, d)
+
+# 위치 알려주기(index)  # 찾는 문자나 문자열이 없으면 오류
+# a = "Life is too short"
+# in1 = a.index('t')
+# # in2 = a.index('k')
+# print(in1)
+
+# 문자열 삽입(join)
+# a = ",".join('abcd')
+# print(a)
+# b = ",".join(['a', 'b', 'c', 'd'])
+# print(b)
+
+# 소문자를 대문자로 바꾸기(upper)
+# a = 'hi'
+# b = a.upper()
+# print(b)
+
+# 대문자를 소문자로 바꾸기(lower)
+# a = "HI"
+# b = a.lower()
+# print(b)
+
+# 왼쪽 공백 지우기(lstrip)"
+# a = " hi "
+# b = a.lstrip()
+# print(b)
+
+# 오른쪽 공백 지우기(rstrip)
+# c = " hi "
+# d = c.rstrip()
+# print(d)
+# 양쪽 공백 지우기(strip)
+# a = " hi "
+# b = a.strip()
+# print(b)
+
+# 문자열 바꾸기(replace)
+# a = "Life is too short"
+# b = a.replace('Life', 'Your leg')
+# print(b)
+
+# 문자열 나누기(split)
+a = "Life is too short"
+b = a.split()   # 공백을 기준으로 문자열을 나눈다
+print(b)
+c = "a:b:c:d"
+d = c.split(":")  # : 기호를 기준으로 문자열을 나눈다
+print(d)
