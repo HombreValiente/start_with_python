7c723f050febf8d66e2865314f468bc1b5f1d7a9
HombreValiente <dohohun@naver.com>
2022-03-25 18:08:44 +0900

    third commit

--- chapter5_1.py
+++ chapter5_1.py
@@ -24,4 +24,8 @@ class Fourcal:
         self.second = second
 # setdata의 첫번째 매개변수 self에는 setdata 매서드를 호출한 객체 a가 자동으로 전달된다.
 a = Fourcal()
-print(a.setdata(4,2))
+b = Fourcal()
+a.setdata(4,2)   # FourCal.setdata(a,4,2)로 해도 동일
+print(a.first)
+b.setdata(3, 7)
+print(b.first)    # 클래스로 만든 객체의 객체변수는 다른 객체의 객체변수 여부에 상관없이 독립적인 값을 유지한다.
